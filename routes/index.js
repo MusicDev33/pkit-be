070558298c4e7437f001ebb7ed52a0b0dbcf642f
cameron <cameron.kueneman@gmail.com>
2023-06-07 02:39:51 -0600

    Working on it - got vscode server script working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Google Sheets Backend' });
+  console.log("Testing this here")
 });
+
+// GET some of that sweet google data
 
 module.exports = router;
