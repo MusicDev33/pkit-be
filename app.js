cd09c0e0544d9c68b2053b41d51c6c97225edc5e
cameron <cameron.kueneman@gmail.com>
2023-06-09 19:49:20 -0600

    good lord it starts to work

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const { google } = require("googleapis");
+const sheets = google.sheets("v4");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let app = express();
+const port = 3000;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/ara", (req, res, next) => {
+  const response = getAraSheet();
+  res.send(response);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
 
-module.exports = app;
+function authorize() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "./SECRET-API-KEY.json",
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+  return auth;
+}
+
+async function getAraSheet() {
+  const auth = authorize();
+  const request = {
+    spreadsheetId: "14TKTeY8TFYASxbetfyKc8GJRDEtp-zIZOEBbRbLkumM",
+    range: "A2:D2",
+    valueRenderOption: "FORMATTED_VALUE",
+    dateTimeRenderOption: "FORMATTED_STRING",
+    auth: auth,
+  };
+
+  try {
+    const response = await sheets.spreadsheets.values.get(request);
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// const testing = getAraSheet();
+// console.log(testing);
